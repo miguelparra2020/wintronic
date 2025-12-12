c563cc16db1617011fa5d48587d4f0267611e12d
miguelyanpo-dev <miguel.yanpo@gmail.com>
2025-12-12 12:52:13 -0500

    add-changes

--- utils/generalConfig.ts
+++ utils/generalConfig.ts
@@ -34,11 +34,11 @@ export const pathsIconos = {
   path2photos: "M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2M14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1M2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1z",
   }
 
-export const linkProducts = "https://www.google.com/";
-export const linkServices = "https://www.google.com/";
-export const linkPhotos = "https://www.google.com/";
-export const LinkWhasapp = "https://wa.link/bwcdrb";
-export const LinkUsuario = "https://www.google.com/";
+export const linkProducts = "https://wa.me/c/573133487191";
+export const linkServices = "https://wa.me/c/573133487191";
+export const linkPhotos = "https://wa.me/c/573133487191";
+export const LinkWhasapp = "https://wa.link/aqi5a7";
+export const LinkUsuario = "https://wa.me/c/573133487191";
 
             
 export const generalConfig = {
--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,9 +5,9 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://micompañía.com", // Change this to your production URL.
+	baseURL: "https://laferiadelaseo.com", // Change this to your production URL.
 	description:
-		"Descripción de la actividad de la compañía", // Change this to be your website's description.
+		"En La Feria del Aseo somos especialistas en la venta de productos y elementos de aseo diseñados para garantizar espacios limpios, seguros y agradables. Ofrecemos una amplia variedad de soluciones para el hogar, oficinas, negocios y empresas, siempre con productos de alta calidad y precios competitivos. Nuestra misión es ayudarte a mantener un entorno impecable con artículos confiables, prácticos y duraderos. Desde insumos básicos hasta productos especializados, contamos con un catálogo completo que se adapta a las necesidades de cada cliente. En La Feria del Aseo, la limpieza no es solo una tarea… es nuestra pasión. Y trabajamos cada día para que tengas acceso a los mejores productos, de manera fácil, rápida y con un servicio al cliente que te hará sentir en casa.", // Change this to be your website's description.
 	type: "website",
 	image: {
 		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
@@ -15,7 +15,7 @@ export const seoConfig = {
 		width: 1200,
 		height: 630
 	},
-	siteName: "Minoxidil Kirkland", // Change this to your website's name,
+	siteName: "La feria del aseo", // Change this to your website's name,
 	twitter: {
 		card: "Descripción de la actividad de la compañía"
 	}
@@ -25,11 +25,12 @@ export const seoConfig = {
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Comercyo", // Change this to your website's name.
-	short_name: "Comercyo", // Change this to your website's short name.
+	name: "La feria del aseo", // Change this to your website's name.
+	short_name: "La feria del aseo", // Change this to your website's short name.
 	description:
-		"Descripción de la actividad de la compañía", // Change this to your websites description.
-	theme_color: "#30E130", // Change this to your primary color.
+		"En La Feria del Aseo somos especialistas en la venta de productos y elementos de aseo diseñados para garantizar espacios limpios, seguros y agradables. Ofrecemos una amplia variedad de soluciones para el hogar, oficinas, negocios y empresas, siempre con productos de alta calidad y precios competitivos. Nuestra misión es ayudarte a mantener un entorno impecable con artículos confiables, prácticos y duraderos. Desde insumos básicos hasta productos especializados, contamos con un catálogo completo que se adapta a las necesidades de cada cliente. En La Feria del Aseo, la limpieza no es solo una tarea… es nuestra pasión. Y trabajamos cada día para que tengas acceso a los mejores productos, de manera fácil, rápida y con un servicio al cliente que te hará sentir en casa.", // Change this to be your website's description.
+	 // Change this to your websites description.
+	theme_color: "#ffffffff", // Change this to your primary color.
 	background_color: "#ffffff", // Change this to your background color.
 	display: "minimal-ui",
 	icons: [
