4b7a1042f379d6c8729e2b1daf1662e50e6b0125
miguelyanpo-dev <miguel.yanpo@gmail.com>
2026-01-20 15:52:21 -0500

    add-changes

--- src/components/global/Componentes/index.ts
+++ src/components/global/Componentes/index.ts
@@ -1,6 +1,7 @@
 export { default as MenuView } from "./MenuView"
 export { default as DescargarApp } from "./DescargarApp"
 export { default as HeroSesion } from "./HeroSesion.astro"
+export { default as Stat } from "./Stat.astro"
 
 
 
--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,4 +1,4 @@
 export { Header } from "./Header"
 export { Layout } from "./Layout"
 export { LayoutGeneral } from "./LayoutGeneral"
-export {  MenuView, DescargarApp, HeroSesion} from "./Componentes"
+export {  MenuView, DescargarApp, HeroSesion, Stat} from "./Componentes"
