a8ffb19505851224f9261e7df33db9e450ad6b47
miguelyanpo-dev <miguel.yanpo@gmail.com>
2026-01-23 08:48:06 -0500

    add-changes

--- src/components/global/Componentes/index.ts
+++ src/components/global/Componentes/index.ts
@@ -2,6 +2,15 @@ export { default as MenuView } from "./MenuView"
 export { default as DescargarApp } from "./DescargarApp"
 export { default as HeroSesion } from "./HeroSesion.astro"
 export { default as Stat } from "./Stat.astro"
+export { default as QuienesSomos } from "./QuienesSomos.astro"
+export { default as NuestrosServicios } from "./NuestrosServicios.astro"
+export { default as NuestrasCategorias } from "./NuestrasCategorias.astro"
+export { default as MaterialesUtilizados } from "./MaterialesUtilizados.astro"
+export { default as ProyectosResultados } from "./ProyectosResultados.astro"
+export { default as ProcesoCreacion } from "./ProcesoCreacion.astro"
+export { default as Beneficios } from "./Beneficios.astro"
+export { default as CierreFinal } from "./CierreFinal.astro"
+export { default as FooterCredits } from "./FooterCredits.astro"
 
 
 
--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,4 +1,7 @@
 export { Header } from "./Header"
 export { Layout } from "./Layout"
 export { LayoutGeneral } from "./LayoutGeneral"
-export {  MenuView, DescargarApp, HeroSesion, Stat} from "./Componentes"
+export {  MenuView, DescargarApp, HeroSesion, Stat, QuienesSomos,
+    NuestrosServicios, NuestrasCategorias, MaterialesUtilizados, ProyectosResultados,
+    ProcesoCreacion, Beneficios, CierreFinal, FooterCredits
+} from "./Componentes"
